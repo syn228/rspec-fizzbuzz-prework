b2a9dbcd2613ca700fb798589d82c80c8d04e4d6
Aaron Na <syn228@nyu.edu>
2018-04-07 22:08:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ def fizzbuzz(number)
     return "Fizz"
   elsif (number % 5 == 0)
     return "Buzz"
-  elsif (number % 5 == 0 && number % 3 == 0)
+  elsif (number % 5 == 0) && (number % 3 == 0)
     return "FizzBuzz"
   else return nil
   end
